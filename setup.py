973318542e8254a04eb631a289bd2db5d95ea520
Henrik <henrik.steude@ginkgo-analytics.com>
2020-10-11 09:52:37 +0200

    initial project structure, setup.py, .gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='com-agents',
+    version='0.0.1',
+    description='Implementation of the paper by Nautrup et. al. 2020',
+    author='Henrik Steude',
+    author_email='henrik.steude@gmail.com',
+    url='https://github.com/hsteude/communicating_agents',
+    packages=find_packages(include=['comm_agents']),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'loguru'],
+    extras_require={'plotting': ['matplotlib', 'jupyter', 'plotly']},
+    setup_requires=['pytest-runner', 'flake8'],
+    tests_require=['pytest']
+)
+
