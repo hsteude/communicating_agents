e7d3d40fc912f3263c909bb233dece2531a31b4a
Henrik <henrik.steude@ginkgo-analytics.com>
2020-10-15 21:37:20 +0200

    pytorch dataset

--- comm_agents/data/datahandler.py
+++ comm_agents/data/datahandler.py
@@ -1,6 +1,44 @@
-class DataSet():
-    def __init__(self, observations=None, questions=None, opt_answers=None, hidden_states=None):
-        self.observations = observations
-        self.questions = questions
-        self.opt_answers = opt_answers
-        self.hidden_states = hidden_states
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+import pandas as pd
+
+PATH = 'data/reference_experiment_dat.csv'
+
+
+class RefExpDataset(Dataset):
+
+    def __init__(self):
+        # read with pandas
+        df = pd.read_csv(PATH, dtype=np.float32)
+        self.n_samples = len(df)
+
+        # define attributes
+        self.hidden_states = torch.from_numpy(
+            df[['m0', 'm1', 'q0', 'q1']].values)
+
+        self.observations = torch.from_numpy(
+            df[[c for c in df.columns if 'o' in c]].values)
+
+        self.questions = torch.from_numpy(
+                df[['m_ref_a', 'v_ref_a', 'm_ref_b', 'v_ref_b']].values)
+
+        self.opt_answers = torch.from_numpy(
+            df[['alpha_star0', 'alpha_star1',
+                'phi_star0', 'phi_star1']].values)
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return (self.hidden_states[index], self.observations[index],
+                self.questions[index], self.opt_answers[index])
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+
+
+if __name__ == '__main__':
+    # create dataset
+    dataset = RefExpDataset()
